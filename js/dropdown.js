2da9ed0418d23b0635ae02a64c6d51997a9602e0
Harris Fauntleroy <HarrisFauntleroy@users.noreply.github.com>
2020-07-09 09:44:46 +1000

    added pages

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,19 +1,8 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+// Get the button, and when the user clicks on it, execute myFunction
+document.getElementById("dropbtn").onclick = function() {myFunction()};
+
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
 }
