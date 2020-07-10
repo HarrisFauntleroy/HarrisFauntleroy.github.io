04921c1c325e3056cbc4d0c5312e6e4c37d96732
Harris Fauntleroy <HarrisFauntleroy@users.noreply.github.com>
2020-07-10 14:12:33 +1000

    added mobile navigation menu

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,3 +1,4 @@
+/* Vertical nac bar */
 var toggler = document.getElementsByClassName("dropdown-btn");
 var i;
 
@@ -5,5 +6,15 @@ for (i = 0; i < toggler.length; i++) {
   toggler[i].addEventListener("click", function() {
     this.parentElement.querySelector(".dropdown-content").classList.toggle("downlist");
     this.classList.toggle("dropdown-btn-down");
+  });
+}
+
+/* Mobile nav bar */
+var toggler2 = document.getElementsByClassName("mobile-nav-btn");
+var mobnav;
+
+for (mobnav = 0; mobnav < toggler2.length; mobnav++) {
+  toggler2[mobnav].addEventListener("click", function() {
+    this.parentElement.querySelector(".mobile-nav-content").classList.toggle("downlist2");
   });
 }
