6ed50d45e0de3a32ca87e0786df48c8c15ac3464
Harris Fauntleroy <HarrisFauntleroy@users.noreply.github.com>
2020-07-10 22:33:54 +1000

    dynamic active link

--- js/dropdown.js
+++ js/dropdown.js
@@ -18,3 +18,29 @@ for (mobnav = 0; mobnav < toggler2.length; mobnav++) {
     this.parentElement.querySelector(".side-nav-content").classList.toggle("downlist2");
   });
 }
+
+/* Quick navigation active page */
+function quickNav() {
+  let current_location = location.pathname.split('/')[1];
+  if (current_location === "") return;
+  let menu_items = document.querySelector("nav").getElementsByTagName("a");
+  for (let i = 0, len = menu_items.length; i < len; i++) {
+    if (menu_items[i].getAttribute("href").indexOf(current_location) !== -1) {
+      menu_items[i].className = "active";
+    }
+  }
+}
+quickNav()
+
+/* Side navigation active page */
+function sideNav() {
+  let current_location = location.pathname.split('/')[1];
+  if (current_location === "") return;
+  let menu_items = document.querySelector("aside").getElementsByTagName("a");
+  for (let i = 0, len = menu_items.length; i < len; i++) {
+    if (menu_items[i].getAttribute("href").indexOf(current_location) !== -1) {
+      menu_items[i].className = "active";
+    }
+  }
+}
+sideNav()
