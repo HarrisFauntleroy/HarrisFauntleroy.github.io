9187c3c72b6b241b003e599a0e9af01428f8867b
Harris Fauntleroy <HarrisFauntleroy@users.noreply.github.com>
2020-07-10 10:41:11 +1000

    new navigations

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,14 +1,9 @@
-var dropdown = document.getElementsByClassName("dropdown-btn");
+var toggler = document.getElementsByClassName("dropdown-btn");
 var i;
 
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".dropdown-content").classList.toggle("downlist");
+    this.classList.toggle("dropdown-btn-down");
   });
 }
