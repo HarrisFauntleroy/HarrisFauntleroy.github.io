389e5b8e65819d997633979e749d3482fbaf2ea1
Harris Fauntleroy <HarrisFauntleroy@users.noreply.github.com>
2020-07-12 18:15:31 +1000

    try again

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,26 +1,3 @@
-/* Desktop navigation sidebar */
-var toggler = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < toggler.length; i++) {
-  toggler[i].addEventListener("click", function() {
-    this.parentElement.querySelector(".dropdown-content").classList.toggle("downlist");
-    this.classList.toggle("dropdown-btn-down");
-  });
-}
-
-/* Mobile navigation sidebar */
-var toggler2 = document.getElementsByClassName("mobile-nav-btn");
-var mobnav;
-
-for (mobnav = 0; mobnav < toggler2.length; mobnav++) {
-  toggler2[mobnav].addEventListener("click", function() {
-    this.parentElement.querySelector(".side-nav-content").classList.toggle("downlist2");
-  });
-}
-
-/* -------------------------------------------------------------------------------- */
-
 /* Quick navigation active page */
 function quickNav() {
   let current_location = location.pathname.split('/')[1];
