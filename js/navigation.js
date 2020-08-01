7f73026e6722abe68b53cb08d86dad2a36f3bd48
Harris Fauntleroy <HarrisFauntleroy@users.noreply.github.com>
2020-08-01 12:51:16 +1000

    Old site becomes this site.

--- js/navigation.js
+++
@@ -1,9 +0,0 @@
-function loadNav() {
-  $("#nav").load("quicknav.html")
-};
-loadNav()
-
-function loadSideNav() {
-  $("#sidebar-left").load("sidenav.html")
-};
-loadSideNav()
--- js/themepref.js
+++
@@ -1,8 +0,0 @@
-/* Check theme preference */
-if (
-  localStorage.getItem("color-mode") === "dark" ||
-  (window.matchMedia("(prefers-color-scheme: dark)").matches &&
-    !localStorage.getItem("color-mode"))
-) {
-  document.documentElement.setAttribute("color-mode", "dark");
-}
