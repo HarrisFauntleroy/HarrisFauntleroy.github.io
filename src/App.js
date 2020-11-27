195a7295ed3a458691bdb746cae85f7706fab8f8
harrisfauntleroy <harrisfauntleroy@gmail.com>
2020-11-27 10:10:43 +1000

    work on basic layout

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import { Navbar, NavItem, NavBurger, DropdownMenu } from './components/Navbar'
 function App() {
 
   return (
-
-    <Navbar>
-      <NavItem icon="HF" location="https://harrisfauntleroy.dev/"/>
-      <NavBurger>
-        <DropdownMenu/>
-      </NavBurger>
-    </Navbar>
+    <div className="content-wrapper">
+      <Navbar>
+        <NavItem icon="HF" location="https://harrisfauntleroy.dev/" />
+        <NavBurger>
+          <DropdownMenu />
+        </NavBurger>
+      </Navbar>
+      <div className="container">
+        <h1>Coming soon...</h1>
+      </div>
+      <footer>&copy; 2020 Harris Fauntleroy</footer>
+    </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,7 @@ import { ReactComponent as HamburgerIcon } from '../icons/hamburger.svg'
 import { ReactComponent as GithubIcon } from '../icons/github-brands.svg'
 import { ReactComponent as DiscordIcon } from '../icons/discord-brands.svg'
 import { ReactComponent as GearIcon } from '../icons/cog-solid.svg'
+import { ReactComponent as PeopleIcon } from '../icons/users-solid.svg'
 import { ReactComponent as ArrowRightIcon } from '../icons/arrow-right-solid.svg'
 import { ReactComponent as ArrowLeftIcon } from '../icons/arrow-left-solid.svg'
 
@@ -92,7 +93,8 @@ export function DropdownMenu() {
                     <DropdownItem
                         leftIcon={<UserIcon />}
                         location="https://harrisfauntleroy.dev/resume/"
-                    >Resume</DropdownItem>
+                    >Resume
+                    </DropdownItem>
 
                     <DropdownItem
                         leftIcon={<GearIcon />}
@@ -101,32 +103,20 @@ export function DropdownMenu() {
                         Projects
           </DropdownItem>
 
-                    <DropdownItem
+          <DropdownItem
+                        leftIcon={<PeopleIcon />}
+                        rightIcon={<ArrowRightIcon />}
+                        goToMenu="personal">
+                        Socials
+          </DropdownItem>
+                    
+          <DropdownItem
                         leftIcon={<LinkIcon />}
                         rightIcon={<ArrowRightIcon />}
                         goToMenu="links">
                         Links
           </DropdownItem>
 
-                </div>
-            </CSSTransition>
-
-            <CSSTransition
-                in={activeMenu === 'links'}
-                timeout={500}
-                classNames="menu-secondary"
-                unmountOnExit
-                onEnter={calcHeight}>
-                <div className="menu">
-                    <DropdownItem goToMenu="main" leftIcon={<ArrowLeftIcon />}>
-                        <h2>Links</h2>
-                    </DropdownItem>
-                    <DropdownItem location="https://github.com/HarrisFauntleroy" leftIcon={<GithubIcon />}>Github</DropdownItem>
-                    <DropdownItem location="https://www.linkedin.com/in/harris-fauntleroy-b4b5b51b1/" leftIcon={<LinkedInIcon />}>LinkedIn</DropdownItem>
-                    <DropdownItem location="https://twitter.com/CosmicOctopus99" leftIcon={<TwitterIcon />}>Twitter</DropdownItem>
-                    <DropdownItem location="https://discord.gg/N4MGJGG7RA" leftIcon={<DiscordIcon />}>Discord</DropdownItem>
-                    <DropdownItem location="https://www.instagram.com/harrisfauntleroy/" leftIcon={<InstagramIcon />}>Instagram</DropdownItem>
-                    <DropdownItem location="https://www.instructables.com/member/HazzWold1993/" leftIcon={<InstructablesIcon />}>Instructables</DropdownItem>
                 </div>
             </CSSTransition>
 
@@ -144,6 +134,43 @@ export function DropdownMenu() {
                     <DropdownItem location="https://github.com/HarrisFauntleroy/no-more-plants" leftIcon="🌿">No More Plants!</DropdownItem>
                     <DropdownItem location="https://github.com/HarrisFauntleroy/library-admin-panel" leftIcon="📖">Library Admin Panel</DropdownItem>
                     <DropdownItem location="https://github.com/HarrisFauntleroy/hermes" leftIcon="🚧">Hermes (In-dev)</DropdownItem>
+                </div>
+            </CSSTransition>
+
+            <CSSTransition
+                in={activeMenu === 'personal'}
+                timeout={500}
+                classNames="menu-secondary"
+                unmountOnExit
+                onEnter={calcHeight}>
+                <div className="menu">
+                    <DropdownItem goToMenu="main" leftIcon={<ArrowLeftIcon />}>
+                        <h2>Socials</h2>
+                    </DropdownItem>
+                    <DropdownItem location="https://github.com/HarrisFauntleroy" leftIcon={<GithubIcon />}>Github</DropdownItem>
+                    <DropdownItem location="https://www.linkedin.com/in/harris-fauntleroy-b4b5b51b1/" leftIcon={<LinkedInIcon />}>LinkedIn</DropdownItem>
+                    <DropdownItem location="https://twitter.com/CosmicOctopus99" leftIcon={<TwitterIcon />}>Twitter</DropdownItem>
+                    <DropdownItem location="https://discord.gg/N4MGJGG7RA" leftIcon={<DiscordIcon />}>Discord</DropdownItem>
+                    <DropdownItem location="https://www.instagram.com/harrisfauntleroy/" leftIcon={<InstagramIcon />}>Instagram</DropdownItem>
+                    <DropdownItem location="https://www.instructables.com/member/HazzWold1993/" leftIcon={<InstructablesIcon />}>Instructables</DropdownItem>
+                </div>
+            </CSSTransition>
+
+            <CSSTransition
+                in={activeMenu === 'links'}
+                timeout={500}
+                classNames="menu-secondary"
+                unmountOnExit
+                onEnter={calcHeight}>
+                <div className="menu">
+                    <DropdownItem goToMenu="main" leftIcon={<ArrowLeftIcon />}>
+                        <h2>Links</h2>
+                    </DropdownItem>
+                    <DropdownItem location="https://mdigi.tools/color-shades/" leftIcon={<GithubIcon />}>Color Shade tool</DropdownItem>
+                    <DropdownItem location="https://unsplash.com/" leftIcon={<LinkedInIcon />}>UnSplash Photos</DropdownItem>
+                    <DropdownItem location="https://tinypng.com/" leftIcon={<TwitterIcon />}>TinyPNG</DropdownItem>
+                    <DropdownItem location="https://fontawesome.com/icons?d=gallery" leftIcon={<DiscordIcon />}>Font Awesome Icons</DropdownItem>
+                    <DropdownItem location="https://cdnjs.com/" leftIcon={<InstagramIcon />}>CDNJS</DropdownItem>
                 </div>
             </CSSTransition>
         </div>
