f9753f3229b9e0ba7676e15228ad41dabf7fafff
harrisfauntleroy <harrisfauntleroy@gmail.com>
2020-11-29 12:50:36 +1000

    more icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import { CSSTransition } from 'react-transition-group'
 import { Navbar, NavItem, NavBurger, DropdownMenu } from './components/Navbar'
 
 function App() {
