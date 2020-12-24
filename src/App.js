11ed35f2b5feef3a1e78494458ae500bdf869384
harrisfauntleroy <harrisfauntleroy@gmail.com>
2020-12-24 19:13:33 +1000

    Lottie animations

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import React from 'react';
-import { Navbar, NavItem, NavBurger, DropdownMenu } from './components/Navbar'
+import "./App.css";
+import React from "react";
+import { Navbar, NavItem, NavBurger, DropdownMenu } from "./components/Navbar";
+import LottieFeature from "./components/Lottie/LottieFeature";
+import lottie3 from "./components/Lottie/6873-under-maintenance.json";
 
 function App() {
-
   return (
-    <div className="content-wrapper">
+    <div className='content-wrapper'>
       <Navbar>
-        <NavItem icon="HF" location="https://harrisfauntleroy.dev/" />
+        <NavItem icon='HF' location='https://harrisfauntleroy.dev/' />
         <NavBurger>
           <DropdownMenu />
         </NavBurger>
       </Navbar>
-      <div className="container">
+      <div className='container'>
         <h1>Coming soon...</h1>
+        <LottieFeature height={120} width={120} lotti={lottie3} />
       </div>
       <footer>&copy; 2020 Harris Fauntleroy</footer>
     </div>
